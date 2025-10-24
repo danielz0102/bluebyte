86608f04abf5bc509e374853b9c814aa394eb5af
Daniel Zambrano <danyzglez@hotmail.com>
2025-10-24 13:06:22 -0600

    Actualizar script sql

--- server/bluebyte.sql
+++ server/bluebyte.sql
@@ -1,16 +1,18 @@
 DROP DATABASE IF EXISTS bluebyte;
+
 CREATE DATABASE bluebyte;
+
 USE bluebyte;
 
 CREATE TABLE users(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
     image LONGTEXT NOT NULL
 );
 
 CREATE TABLE posts(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     isDraft BOOLEAN DEFAULT FALSE,
@@ -18,9 +20,7 @@ CREATE TABLE posts(
     updatedAt DATETIME,
     image LONGTEXT NOT NULL,
     userId INT NOT NULL,
-    
-    CONSTRAINT fk_posts_userId FOREIGN KEY(userId)
-    REFERENCES users(id)
+    CONSTRAINT fk_posts_userId FOREIGN KEY(userId) REFERENCES users(id)
 );
 
 CREATE TABLE categories(
@@ -29,21 +29,15 @@ CREATE TABLE categories(
     description VARCHAR(255) NOT NULL,
     image LONGTEXT NOT NULL,
     userId INT NOT NULL,
-    
-    CONSTRAINT fk_categories_userId FOREIGN KEY(userId)
-    REFERENCES users(id)
+    CONSTRAINT fk_categories_userId FOREIGN KEY(userId) REFERENCES users(id)
 );
 
 CREATE TABLE posts_categories(
     id INT AUTO_INCREMENT PRIMARY KEY,
     postId INT NOT NULL,
     categoryId INT NOT NULL,
-    
-    CONSTRAINT fk_posts_categories_postId FOREIGN KEY(postId)
-    REFERENCES posts(id),
-    
-    CONSTRAINT fk_posts_categories_categoryId FOREIGN KEY(categoryId)
-    REFERENCES categories(id)
+    CONSTRAINT fk_posts_categories_postId FOREIGN KEY(postId) REFERENCES posts(id),
+    CONSTRAINT fk_posts_categories_categoryId FOREIGN KEY(categoryId) REFERENCES categories(id)
 );
 
 CREATE TABLE comments(
@@ -52,12 +46,8 @@ CREATE TABLE comments(
     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
     userId INT NOT NULL,
     postId INT NOT NULL,
-    
-    CONSTRAINT fk_comments_userId FOREIGN KEY(userId)
-    REFERENCES users(id),
-    
-    CONSTRAINT fk_comments_postId FOREIGN KEY(postId)
-    REFERENCES posts(id)
+    CONSTRAINT fk_comments_userId FOREIGN KEY(userId) REFERENCES users(id),
+    CONSTRAINT fk_comments_postId FOREIGN KEY(postId) REFERENCES posts(id)
 );
 
 CREATE TABLE notifications (
@@ -65,39 +55,172 @@ CREATE TABLE notifications (
     message VARCHAR(255) NOT NULL,
     seen BOOLEAN DEFAULT FALSE,
     userId INT NOT NULL,
-    
-    CONSTRAINT fk_notifications_userId FOREIGN KEY (userId) 
-    REFERENCES users(id)
+    CONSTRAINT fk_notifications_userId FOREIGN KEY (userId) REFERENCES users(id)
 );
 
-DELIMITER //
-
-CREATE PROCEDURE login(
-	IN p_username VARCHAR(50),
+-- STORED PROCEDURES
+DELIMITER / / CREATE PROCEDURE login(
+    IN p_username VARCHAR(50),
     IN p_password VARCHAR(50)
-)
-BEGIN
-	SELECT id, username, image FROM users 
-    WHERE username = p_username AND password = p_password
-    LIMIT 1;
-END //
+) BEGIN
+SELECT
+    id,
+    username,
+    image
+FROM
+    users
+WHERE
+    username = p_username
+    AND password = p_password
+LIMIT
+    1;
 
-CREATE PROCEDURE registerUser(
+END / / CREATE PROCEDURE registerUser(
     IN p_username VARCHAR(50),
     IN p_password VARCHAR(50),
     IN p_image LONGTEXT
-)
-BEGIN
-    IF EXISTS (SELECT 1 FROM users WHERE username = p_username) THEN
-        SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'Username already exists';
-    ELSE
-        INSERT INTO users(username, password, image)
-        VALUES(p_username, p_password, p_image);
+) BEGIN IF EXISTS (
+    SELECT
+        1
+    FROM
+        users
+    WHERE
+        username = p_username
+) THEN SIGNAL SQLSTATE '45000'
+SET
+    MESSAGE_TEXT = 'Username already exists';
 
-        SELECT id, username, image FROM users 
-        WHERE id = LAST_INSERT_ID();
-    END IF;
-END //
+ELSE
+INSERT INTO
+    users(username, password, image)
+VALUES
+(p_username, p_password, p_image);
 
-DELIMITER ;
+SELECT
+    id,
+    username,
+    image
+FROM
+    users
+WHERE
+    id = LAST_INSERT_ID();
+
+END IF;
+
+END / / CREATE PROCEDURE getLastestPosts() BEGIN
+SELECT
+    *
+FROM
+    posts
+ORDER BY
+    COALESCE(updatedAt, publishedAt) DESC
+LIMIT
+    10;
+
+END / / CREATE PROCEDURE createPost(
+    IN p_title VARCHAR(255),
+    IN p_content TEXT,
+    IN p_image LONGTEXT,
+    IN p_userId INT
+) BEGIN IF NOT EXISTS (
+    SELECT
+        1
+    FROM
+        users
+    WHERE
+        id = p_userId
+) THEN SIGNAL SQLSTATE '45000'
+SET
+    MESSAGE_TEXT = 'User does not exist';
+
+ELSE
+INSERT INTO
+    posts(title, content, publishedAt, image, userId)
+VALUES
+(p_title, p_content, NOW(), p_image, p_userId);
+
+SELECT
+    *
+FROM
+    posts
+WHERE
+    id = LAST_INSERT_ID();
+
+END IF;
+
+END / / CREATE PROCEDURE updatePost(
+    IN p_id INT,
+    IN p_title VARCHAR(255),
+    IN p_content TEXT,
+    IN p_image LONGTEXT
+) BEGIN IF NOT EXISTS (
+    SELECT
+        1
+    FROM
+        posts
+    WHERE
+        id = p_id
+) THEN SIGNAL SQLSTATE '45000'
+SET
+    MESSAGE_TEXT = 'Post does not exists';
+
+END IF;
+
+UPDATE
+    posts
+SET
+    title = p_title,
+    content = p_content,
+    image = p_image
+WHERE
+    id = p_id;
+
+SELECT
+    *
+FROM
+    posts
+WHERE
+    id = p_id;
+
+END / / CREATE PROCEDURE deletePost(IN p_id INT) BEGIN
+DELETE FROM
+    posts
+WHERE
+    id = p_id;
+
+END / / CREATE PROCEDURE registerCategory(
+    IN p_title VARCHAR(255),
+    IN p_description VARCHAR(255),
+    IN p_image LONGTEXT,
+    IN p_userId INT
+) BEGIN IF EXISTS (
+    SELECT
+        1
+    FROM
+        categories
+    WHERE
+        title = p_title
+) THEN SIGNAL SQLSTATE '45000'
+SET
+    MESSAGE_TEXT = 'Category already exists';
+
+ELSE
+INSERT INTO
+    categories(title, description, image, userId)
+VALUES
+(p_title, p_description, p_image, p_userId);
+
+SELECT
+    id,
+    title,
+    description,
+    image,
+    userId
+FROM
+    categories
+WHERE
+    id = LAST_INSERT_ID();
+
+END IF;
+
+END // DELIMITER;
